739c228a5fdfcce67f2ad1a2525b81dec8186615
hyoni0817 <dudu1104@naver.com>
2020-10-25 18:14:32 +0900

    Add: Head, AppLayout

---
+++ front/pages/_app.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Head from 'next/head';
+import AppLayout from '../components/AppLayout';
+import '../public/css/style.css';
+
+const StudyPlanner = ({ Component, pageProps }) => {
+    return (
+        <>
+            <Head>
+                <title>Study Planner</title>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/antd/4.7.2/antd.min.css" />
+            </Head>
+            <AppLayout>
+                <Component {...pageProps} />
+            </AppLayout>
+        </>
+    )
+}
+
+StudyPlanner.propTypes = {
+    Component: PropTypes.elementType.isRequired,
+    pageProps: PropTypes.object.isRequired,
+}
+
+export default StudyPlanner;
