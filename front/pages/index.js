3722096bd8e50f5ec727473d7e962bb4ec8b8808
hyoni0817 <dudu1104@naver.com>
2020-11-05 23:55:11 +0900

    Add: 홈페이지

---
+++ front/pages/index.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { Layout, Card, Col, Row, Progress, Button } from 'antd';
+import Link from 'next/link';
+import TodayList from '../components/TodayList';
+import Dday from '../components/Dday';
+import SelectForms from '../components/SelectForms';
+import { FormOutlined } from '@ant-design/icons';
+
+const Home = () => {
+    const date = new Date();
+    const days = ["일", "월", "화", "수", "목", "금", "토"];
+    const [ size, setSize ] = useState('large');
+    
+    const handleSizeChange = e => {
+        setSize(e.target.value);
+    };
+
+    return (
+        <>
+            <div>{date.getFullYear()}년 {date.getMonth()}월 {date.getDate()}일 {days[date.getDay()]}요일</div>
+            <Dday />
+            오늘의 성취율
+            <Progress
+                strokeColor={{
+                    from: '#108ee9',
+                    to: '#87d068',
+                }}
+                percent={99.9}
+                status="active"
+            />
+            <SelectForms />
+            <TodayList />
+            <Button type="primary" shape="circle" size={size}>
+                <Link href="/createplan"><a><FormOutlined /></a></Link>
+            </Button>
+        </>
+    )
+};
+
+export default Home;
