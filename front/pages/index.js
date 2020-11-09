2d1bb43354ac5d7d598c0b4c73c6b4d0349436b3
hyoni0817 <dudu1104@naver.com>
2020-11-10 01:12:47 +0900

    Add: 홈화면에서 글쓰기 버튼을 눌렀을 때 modal 띄우기
    - 직접 url로 이동하는 것이 아닌, /createplan으로 이동해서 modal이 뜬 것처럼 함.

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,15 +1,39 @@
 import React, { useState } from 'react';
-import { Layout, Card, Col, Row, Progress, Button } from 'antd';
-import Link from 'next/link';
+import { Progress, Button, Modal } from 'antd';
 import TodayList from '../components/TodayList';
 import Dday from '../components/Dday';
 import SelectForms from '../components/SelectForms';
 import { FormOutlined } from '@ant-design/icons';
+import styled from 'styled-components';
 
-const Home = () => {
+const ModalWrapper = styled(Modal)`
+    @media(max-width: 767px) {
+        display: none;
+    }   
+`;
+
+const Home = (props) => {
     const date = new Date();
     const days = ["일", "월", "화", "수", "목", "금", "토"];
     const [ size, setSize ] = useState('large');
+    const [ visible, setVisible ] = useState(false);
+
+    const showModal = () => {
+        window.history.pushState(null, null, '/createplan');
+        setVisible(true);
+    };
+
+    const handleOk = (e) => {
+        window.history.pushState(null, null, '/');
+        console.log(e);
+        setVisible(false);
+    };
+
+    const handleCancel = (e) => {
+        window.history.pushState(null, null, '/');
+        setVisible(false);
+        console.log(e);
+    };
 
     return (
         <>
@@ -26,9 +50,17 @@ const Home = () => {
             />
             <SelectForms />
             <TodayList />
-            <Button type="primary" shape="circle" size={size}>
-                <Link href="/createplan"><a><FormOutlined /></a></Link>
+            <Button type="primary" shape="circle" size={size} onClick={showModal}>
+                <a><FormOutlined /></a>
             </Button>
+            <ModalWrapper
+                title="작성 하기"
+                visible={visible}
+                onOk={handleOk}
+                onCancel={handleCancel}
+            >
+                <SelectForms />
+            </ModalWrapper>
         </>
     )
 };
