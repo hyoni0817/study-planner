9ad11d136c2b26b24fb78c2a7a75953aab802ce5
hyoni0817 <dudu1104@naver.com>
2020-11-13 23:53:56 +0900

    Add: Portal을 사용한 MobileForm 컴포넌트
    - 모바일 화면 크기에 맞는 form을 보여주는데 다른 컴포넌트 위에 겹쳐져서 표현해야해서 Portal을 사용함.

---
+++ front/components/MobileFormPortal.js
@@ -0,0 +1,16 @@
+import { useEffect, useRef, useState } from 'react';
+import ReactDOM from 'react-dom'; 
+
+const MobileFormPortal = ({ children, selector }) => {
+  const ref = useRef();
+  const [mounted, setMounted] = useState(false);
+  
+  useEffect(() => {
+    ref.current = document.querySelector(selector)
+    setMounted(true);
+  });
+
+  return mounted ? ReactDOM.createPortal(children, ref.current) : null;
+};
+
+export default MobileFormPortal;
