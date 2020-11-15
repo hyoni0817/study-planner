8f17bcb75ab466ddedf4e7e0f2e6320e6821b66f
hyoni0817 <dudu1104@naver.com>
2020-11-16 02:03:28 +0900

    Move: 모달 관련 코드를 DesktopForm.js로 이동
    - 코드 이동 후 DesktopFrom 컴포넌트를 불러옴.

--- front/pages/createplan.js
+++ front/pages/createplan.js
@@ -1,16 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { Modal, Button } from 'antd';
-import SelectForms from '../components/SelectForms';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import { useRouter } from 'next/router';
 import MobileForm from '../containers/MobileForm';
 import MobileFormPortal from '../components/MobileFormPortal';
-
-const ModalWrapper = styled(Modal)`
-    @media(max-width: 767px) {
-        display: none;
-    }   
-`;
+import DesktopForm from '../containers/DesktopForm';
 
 const MobileFormWrapper = styled(MobileForm)`
     @media(min-width: 768px) {
@@ -18,35 +11,19 @@ const MobileFormWrapper = styled(MobileForm)`
     }
 `;
 
-const CreatePlan = ({state}) => {
+const CreatePlan = () => {
     const router = useRouter()
 
     useEffect(() => {
       router.prefetch('/')
     }, [])
 
-    const handleOk = (e) => {
-        console.log(e);
-    };
-
-    const handleCancel = (e) => {
-        router.push('/');
-        console.log(e);
-    };
-
     return (
         <>
             <MobileFormPortal selector="#mobile-form">
                 <MobileFormWrapper />
             </MobileFormPortal>
-            <ModalWrapper
-                title="작성 하기"
-                visible={true}
-                onOk={handleOk}
-                onCancel={handleCancel}
-            >
-                <SelectForms />
-            </ModalWrapper>
+            <DesktopForm />
         </>
     );
 };
