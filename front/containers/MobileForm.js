476a8dbdfa96241934ad12a3e2a0ffdf3ec6972e
hyoni0817 <dudu1104@naver.com>
2020-11-15 20:39:48 +0900

    Fix: 모바일용 폼이 #__next인 div 위에 나타낼 수 있게 css 속성 수정
    - 모바일용 폼은 Portal을 통해 div(#mobile-form)태그에 표현됨.

--- front/containers/MobileForm.js
+++ front/containers/MobileForm.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { PageHeader, Descriptions } from 'antd';
+import { PageHeader } from 'antd';
 import styled from 'styled-components';
 import SelectForms from '../components/SelectForms';
 
 const PageHeaderWrapper = styled.div`
         background-color: #f5f5f5;
-        padding: 24px;
-
-        @media(min-width: 768px) {
-            display: none;
-        }   
+        padding: 24px;       
+        position: fixed;
+        top: 0;
+        left: 0;
+        height: 100%;
+        width: 100%;
+        overflow:auto;
 `;
 
 const MobileForm = () => {
