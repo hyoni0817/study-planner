24cba7e83cfa6fbab0103154b7fbead3f603b535
hyoni0817 <dudu1104@naver.com>
2020-11-24 17:01:16 +0900

    Add: Redux

---
+++ front/reducers/index.js
@@ -0,0 +1,21 @@
+import { HYDRATE } from 'next-redux-wrapper';
+import { combineReducers } from 'redux';
+
+import todo from './todo';
+
+// (이전상태, 액션) => 다음 상태
+const rootReducer = (state, action) => {
+    switch (action.type) {
+        case HYDRATE:
+            console.log('HYDRATE', action);
+            return action.payload;
+        default: {
+            const combineReducer = combineReducers({
+                todo,
+            });
+            return combineReducer(state, action);
+        }
+    }
+}
+
+export default rootReducer;
