f7d488bc50dbac5aeb1fde8a3c253f62c28d1398
hyoni0817 <dudu1104@naver.com>
2020-12-04 23:55:31 +0900

    Add: 폼 데이터 관련 state, dispatch, onChange 함수

--- front/containers/DdayForm.js
+++ front/containers/DdayForm.js
@@ -1,19 +1,53 @@
 import React, { useState } from 'react';
 import { Form, DatePicker, Input, Button } from 'antd';
+import { useRouter } from 'next/router';
 
-function onChange(date, dateString) {
-  console.log(date, dateString);
-}
+//redux
+import { useDispatch, useSelector } from 'react-redux';
+import { ADD_DDAY } from '../reducers/todo';
+
 
 const DdayForm = () => {
     const [componentSize, setComponentSize] = useState('default');
+    const router = useRouter();
+    const dispatch = useDispatch();
+    const { DdayPostId } = useSelector(state => state.todo);
+
+    const [ DdayId, setDdayId ] = useState(DdayPostId);
+    const [ title, setTitle ] = useState('');
+    const [ contents, setContents ] = useState('');
+    const [ dueDate, setDueDate ] = useState('');
 
     const onFormLayoutChange = ({ size }) => {
         setComponentSize(size);
     };
 
+    const onChangeTitle = (e) => {
+        setTitle(e.target.value);
+    };
+
+    const onChangeContent = (e) => {
+        setContents(e.target.value);
+    };
+
+    const onChangeDueDate = (date, dateString) => {
+        setDueDate(dateString);
+    };
+
     const onFinish = values => {
         console.log('Received values of form: ', values);
+        setDdaysId( DdayId => DdayId + 1);
+        dispatch({
+            type: ADD_DDAY,
+            data: {
+                DdayPostId: DdayId,
+                title,
+                contents,
+                dueDate,
+            }
+        });
+
+        return router.push('/');
     };
 
     return (
@@ -36,7 +70,7 @@ const DdayForm = () => {
                     noStyle
                     rules={[{ required: true, message: '제목을 입력해주세요' }]}
                     >
-                        <Input style={{ width: 160 }} placeholder="D-day 제목을 입력해주세요" />
+                        <Input style={{ width: 160 }} placeholder="D-day 제목을 입력해주세요" value={title} onChange={onChangeTitle} />
                     </Form.Item>
                 </Form.Item>
                 <Form.Item label="내용" colon={false}>
@@ -44,7 +78,7 @@ const DdayForm = () => {
                     name="todocontents"
                     noStyle
                     >
-                        <Input style={{ width: 160 }} placeholder="추가하고 싶은 내용이 있다면 입력해주세요" />
+                        <Input style={{ width: 160 }} placeholder="추가하고 싶은 내용이 있다면 입력해주세요" value={contents} onChange={onChangeContent} />
                     </Form.Item>
                 </Form.Item>
                 <Form.Item label="날짜" colon={false}>
@@ -53,7 +87,7 @@ const DdayForm = () => {
                     noStyle
                     rules={[{ required: true, message: '날짜를 입력해주세요' }]}
                     >
-                        <DatePicker onChange={onChange} />
+                        <DatePicker onChange={onChangeDueDate} />
                     </Form.Item>
                 </Form.Item>
                 <Form.Item label=" " colon={false}>
