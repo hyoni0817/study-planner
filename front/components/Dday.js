2c010d691d58002c8d58eb1fd53c55422524fd72
hyoni0817 <dudu1104@naver.com>
2020-12-05 19:27:30 +0900

    Refactor: DdayList에 저장된 데이터 기반으로 컨텐츠를 나타낼 수 있게 수정

--- front/components/Dday.js
+++ front/components/Dday.js
@@ -1,31 +1,32 @@
 import React from 'react';
 import { Card, Col, Row } from 'antd';
 
-const Dday = () => {
+const Dday = ({post}) => {
+    const calculateDday = () => {
+        const dateArr = post.dueDate.split('-');
+        const now = new Date();
+        const Dday = new Date(dateArr[0], dateArr[1]-1, dateArr[2]);
+
+        const gap = now.getTime() - Dday.getTime();
+        const remainDay = Math.floor(gap / (1000 * 60 * 60 * 24)) * -1; 
+        return remainDay === 0 ? '-day' : ( remainDay < 0 ? `+${remainDay * -1}` : `-${remainDay}` );
+    };
+
     return (
         <>
             <div style={{ padding: 15, textAlign: 'center' }}>
                 <Row gutter={16} justify="center">
                     <Col xs={12} sm={6} md={10} lg={6} >
-                        <Card bordered={false}>
-                            <p>D-10</p>
-                            <span>영어 듣기 시험</span>
-                        </Card>
-                    </Col>
-                    <Col xs={12} sm={6} md={10} lg={6}>
-                        <Card bordered={false}>
-                            <p>D-30</p>
-                            <span>수학 쪽지 시험</span>
-                        </Card>
-                    </Col>
-                    <Col xs={12} sm={6} md={10} lg={6}>
-                        <Card bordered={false}>
-                            <p>D-100</p>
-                            <span>영어 듣기 시험</span>
-                        </Card>
+                        {
+                            post === undefined ? '' : 
+                            <Card bordered={false}>
+                                <p>D{ calculateDday() }</p>
+                                <span>{post.title}</span>
+                            </Card> 
+                        }
                     </Col>
                 </Row>
-            </div>
+            </div> 
         </>
     )
 };
