ee1256e220b62ec75a52a68fccba3bb7e8562f6f
hyoni0817 <dudu1104@naver.com>
2020-12-06 19:28:03 +0900

    Move: 드롭다운 메뉴와 체크 박스 관련 이벤트 핸들러를 컴포넌트 안으로 이동

--- front/components/Todo.js
+++ front/components/Todo.js
@@ -2,35 +2,34 @@ import React from 'react';
 import { Card, Menu, Dropdown, Button, message, Tooltip, Checkbox, Tag } from 'antd';
 import { DownOutlined, UserOutlined, StarFilled } from '@ant-design/icons';
 
-const handleButtonClick = (e) => {
-    message.info('Click on left button.');
-    console.log('click left button', e);
-  }
-  
-const handleMenuClick = (e) => {
-    message.info('Click on menu item.');
-    console.log('click', e);
-}
-
-const onChange = (e) => {
-    console.log(`checked = ${e.target.checked}`);
-}
-
-const menu = (
-    <Menu onClick={handleMenuClick}>
-      <Menu.Item key="1" icon={<UserOutlined />}>
-        1st menu item
-      </Menu.Item>
-      <Menu.Item key="2" icon={<UserOutlined />}>
-        2nd menu item
-      </Menu.Item>
-      <Menu.Item key="3" icon={<UserOutlined />}>
-        3rd menu item
-      </Menu.Item>
-    </Menu>
-);
-
 const Todo = ({post}) => {
+    const handleButtonClick = (e) => {
+      message.info('Click on left button.');
+      console.log('click left button', e);
+    }
+
+    const handleMenuClick = (e) => {
+      message.info('Click on menu item.');
+      console.log('click', e);
+    }
+
+    const onChange = (e) => {
+      console.log(`checked = ${e.target.checked}`);
+    }
+
+    const menu = (
+      <Menu onClick={handleMenuClick}>
+        <Menu.Item key="1" icon={<UserOutlined />}>
+          1st menu item
+        </Menu.Item>
+        <Menu.Item key="2" icon={<UserOutlined />}>
+          2nd menu item
+        </Menu.Item>
+        <Menu.Item key="3" icon={<UserOutlined />}>
+          3rd menu item
+        </Menu.Item>
+      </Menu>
+    );
     return (
         <>
           {
