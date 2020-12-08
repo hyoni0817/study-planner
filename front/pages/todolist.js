2a2cbfa3be0eab78c531dc391bbf696f938d5873
hyoni0817 <dudu1104@naver.com>
2020-12-08 20:32:55 +0900

    Add: '공부 계획'과 'D-day'를 탭으로 나눠서 표현하는 코드

--- front/pages/todolist.js
+++ front/pages/todolist.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import { Tabs } from 'antd';
 import TodoFilter from '../containers/TodoFilter';
 import Todo from '../components/Todo';
+import Dday from '../components/Dday';
+
+const { TabPane } = Tabs;
+
 const TodoList = () => {   
     return (
         <> 
             <div>
                 <TodoFilter />
-                <Todo />
+                <Tabs defaultActiveKey="1">
+                    <TabPane tab="공부 계획" key="1">
+                        <Todo />
+                    </TabPane>
+                    <TabPane tab="D-day" key="2">
+                        <Dday />
+                    </TabPane>
+                </Tabs>
             </div>
         </>
     )
