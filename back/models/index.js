01322c2cec8d277b7c7d238ff90f3b9786dc78e6
hyoni0817 <dudu1104@naver.com>
2021-01-10 00:54:26 +0900

    Refactor: 모델 접근 및 정의 부분 코드 수정
    - 동작은 같음.

--- back/models/index.js
+++ back/models/index.js
@@ -1,13 +1,21 @@
 const Sequelize = require('sequelize');
+const todo = require('./todo');
+const dday = require('./dday');
+const user = require('./user');
+
 const env = process.env.NODE_ENV || 'development';
 const config = require('/../config/config.json')[env];
 const db = {};
 
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
-db.Todo = require('./todo')(sequelize, Sequelize);
-db.Dday = require('./Dday')(sequelize, Sequelize);
-db.User = require('./User')(sequelize, Sequelize);
+db.Todo = todo;
+db.Dday = dday;
+db.User = user;
+
+Object.keys(db).forEach(modelName => {
+  db[modelName].init(sequelize);
+});
 
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
