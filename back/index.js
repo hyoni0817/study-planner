80e94542646f85869e42b96fb1cfc2a4f65913ce
hyoni0817 <dudu1104@naver.com>
2021-01-22 01:52:36 +0900

    Add: 테이블 생성, req.body 사용을 위한 코드, todo 관련 라우터 추가

--- back/index.js
+++ back/index.js
@@ -1,14 +1,28 @@
 const express = require('express'); //서버를 구성해주는 프레임워크
+const morgan = require('morgan');
+const cors = require('cors');
+
+const db = require('./models');
+const todoAPIRouter = require('./routes/todo');
+const todoListAPIRouter = require('./routes/todoList');
 
 const app = express(); //express를 불러와서 실행을 해주면 app이라는 객체가 생김.
+db.sequelize.sync();
 
+app.use(morgan('dev'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+    orgin: 'http://localhost:3000',
+    credentials: true,
+}));
 app.get('/', (req, res) => { //프론트에서 / 서버에 요청을 하면 res.send를 통해 응답을 함.
-    res.send('hello, server');
+    res.send('study planner 백엔드 정상 동작!');
 });
 
-app.get('/about', (req, res) => {
-    res.send('Hello, about');
-})
+app.use('/api/todo', todoAPIRouter);
+app.use('/api/todolist', todoListAPIRouter);
 
 app.listen(3065, () => {
     console.log('server is running on http://localhost:8080');
