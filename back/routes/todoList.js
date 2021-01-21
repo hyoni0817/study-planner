d68e933585a046661036ea7cb1e93e40d9a86d07
hyoni0817 <dudu1104@naver.com>
2021-01-22 01:55:36 +0900

    Add: 작성된 Todo 모두 가져오는 라우터

---
+++ back/routes/todoList.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const router = express.Router();
+const db = requrie('../moldes');
+
+router.get('/', async (req, res, next) => {
+    try {
+        const todoList = await db.Todo.findAll({
+            where: {},
+            attributes: ['title', 'subject', 'quantity', 'unit', 'important', 'completion', 'createAt'],
+        });
+
+        res.json(todoList);
+    } catch (e) {
+        console.error(e);
+        return next(e);
+    }
+})
