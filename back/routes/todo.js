e0e0fe4035712fdc90243bb4435413957ae70943
hyoni0817 <dudu1104@naver.com>
2021-01-21 23:52:10 +0900

    Add: todo 추가하는 라우터

---
+++ back/routes/todo.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const db = require('../models');
+
+const router = express.Router();
+
+router.post('/', async (req, res, next) => {
+    try {
+        const newTodo = await db.Todo.create({
+            title: req.body.title,
+            subject: req.body.subject,
+            quantity: req.body.quantity,
+            unit: req.body.unit,
+            important: req.body.important,
+        });
+        console.log(newTodo);
+        return res.status(200).json(newTodo);
+    } catch (e) {
+        console.error(e);
+        return next(e);
+    }
+});
