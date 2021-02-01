99a29672346b156ae5b5b071502d2a192f1ced7b
hyoni0817 <dudu1104@naver.com>
2021-02-02 02:13:42 +0900

    Add: 쓰기 버튼 고정을 위해 Affix 추가 및 버튼 위치를 오른쪽 정렬

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,19 +1,25 @@
-import React from 'react';
-import { Progress, Button } from 'antd';
+import React, { useState } from 'react';
+import { Progress, Button, Affix } from 'antd';
 import Todo from '../components/Todo';
 import Dday from '../components/Dday';
 import SelectForms from '../components/SelectForms';
 import { FormOutlined } from '@ant-design/icons';
 import {useRouter} from 'next/router';
 import { useSelector } from 'react-redux';
+import styled from 'styled-components';
 
 const Home = (props) => {
     const router = useRouter();
     const { todoList } = useSelector( state => state.todo )
+    const [bottom, setBottom] = useState(10);
     
     const date = new Date();
     const days = ["일", "월", "화", "수", "목", "금", "토"];
 
+    const onClickWriteBtn = () => { 
+        setBottom(bottom + 10);
+        router.push('/createplan');
+    }
     return (
         <>
             <div>{date.getFullYear()}년 {date.getMonth()+1}월 {date.getDate()}일 {days[date.getDay()]}요일</div>
@@ -36,9 +42,11 @@ const Home = (props) => {
                 }) 
             }
             
-            <Button type="primary" shape="circle" size="large" onClick={()=>router.push('/createplan')}>
-                <a><FormOutlined /></a>
-            </Button>
+            <Affix offsetBottom={bottom}>
+                <Button type="primary" shape="circle" size="large" onClick={onClickWriteBtn} style={{float: 'right'}}>
+                    <a><FormOutlined /></a>
+                </Button>
+            </Affix>
         </> 
     )
 };
