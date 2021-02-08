6ef157fb59082f3894a61eaa6336396ad2a02902
hyoni0817 <dudu1104@naver.com>
2021-02-08 18:40:02 +0900

    Refactor: Dday 컴포넌트 전면 수정 - Ant design의 Card 속성을 사용하지 않아 불필요하다고 느낌. 그래서 div 태그 사용하여 D-day 표현함. - D-day가 추가로 있다는 것을 표현하기 위해 padding과 marginRight 값을 수정함.

--- front/components/Dday.js
+++ front/components/Dday.js
@@ -1,14 +1,34 @@
 import React from 'react';
-import { Card, Col, Row } from 'antd';
+import { Col, Row } from 'antd';
 import { useSelector } from 'react-redux';
 import styled from 'styled-components';
 
-const DdayBox = styled(Card)`
+const DdayBox = styled.div`
     background: linear-gradient(to right, #3a1c71, #d76d77, #ffaf7b);
     border-radius: 5px;
     color: #ffffffe8;
+    padding: 12px;
 `;
 
+const DdayContent = styled.p`
+    font-size: 25px;
+    margin-top: 0;
+    margin-bottom: 0;
+    text-align: left;
+    font-family: 'Fredoka One', cursive;
+`;
+
+const DdayTitle = styled.p`
+    font-size: 13px;
+    text-align: left;
+    margin-bottom: 0;
+`;
+
+const DdayDate = styled.p`
+    font-size: 13px;
+    text-align: right;
+    margin-bottom: 7px;
+`
 const Dday = () => {
     const { DdayList } = useSelector( state => state.todo )
 
@@ -19,20 +39,21 @@ const Dday = () => {
 
         const gap = now.getTime() - Dday.getTime();
         const remainDay = Math.floor(gap / (1000 * 60 * 60 * 24)) * -1; 
-        return remainDay === 0 ? '-day' : ( remainDay < 0 ? `+${remainDay * -1}` : `-${remainDay}` );
+        return remainDay === 0 ? ' - day' : ( remainDay < 0 ? `+${remainDay * -1}` : ` - ${remainDay}` );
     };
 
     return (
         <>
-            <div style={{ padding: 15, textAlign: 'center', overflowX: 'auto', }}>
-                <Row gutter={16} justify="center">
+            <div style={{ padding: '15px 15px 15px 0', textAlign: 'center', overflowX: 'auto', }}>
+                <Row gutter={16} justify="center" style={{ marginRight: '0', }}>
                     { 
                         DdayList.map((c) => {
                             return (
                                 <Col xs={12} sm={6} md={10} lg={6} >
                                     <DdayBox bordered={false}>
-                                        <p>D{ calculateDday(c.dueDate) }</p>
-                                        <span>{c.title}</span>
+                                        <DdayDate>{c.dueDate} 까지</DdayDate>
+                                        <DdayTitle>{c.title}</DdayTitle>
+                                        <DdayContent>D{ calculateDday(c.dueDate) }</DdayContent>
                                     </DdayBox> 
                                 </Col>
                             )
