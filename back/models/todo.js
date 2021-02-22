32ad86fdc83c2d82a2016d34a8b07888174b44c3
hyoni0817 <dudu1104@naver.com>
2021-02-23 00:47:51 +0900

    Refactor: sequelize 버전 업데이트로 인해 코드 전면 수정
    - 모델 정의할 때 기존에 작성했던 문법은 v5 이고, 새로 작성한 문법은 v6 임.
    - v6에서 model 클래스를 사용할 수 있게 됨.

--- back/models/todo.js
+++ back/models/todo.js
@@ -1,50 +1,54 @@
-const { sequelize } = require(".");
+const DataTypes = require('sequelize');
+const { Model } = DataTypes;
 
-module.exports = ( sequelize, DataTypes ) => {
-    const Todo = sequelize.define( 'Todo', {
-        title: {
-            type: DataTypes.STRING(30),
-            allowNull: false,
-        },
-        subject: {
-            type: DataTypes.STRING(30),
-            allowNull: false,
-        },
-        quantity: {
-            type: DataTypes.INTEGER(3),
-            allowNull: false,
-        },
-        unit: {
-            type: DataTypes.STRING(10),
-            allowNull: false,
-        },
-        startTime: {
-            type: DataTypes.STRING(5),
-            allowNull: false,
-            defaultValue: 'none',
-        },
-        endTime: {
-            type: DataTypes.STRING(5),
-            allowNull: false,
-            defaultValue: 'none',
-        },
-        allDayStaus: {
-            type: DataTypes.boolean(false),
-            allowNull: false,
-        },
-        important: {
-            type: DataTypes.BOOLEAN,
-            allowNull: false,
-            defaultValue: false,
-        },
-    }, {
-        charset: 'utf8mb4',
-        collate: 'utf8mb4_general_ci',
-    });
-
-    Todo.associate = (db) => {
-        db.Todo.belongsTo(db.User);
-    };
-    
-    return Todo;
-}
+module.exports = class Todo extends Model {
+    static init(sequelize) {
+        return super.init({
+            title: {
+                type: DataTypes.STRING(30),
+                allowNull: false,
+            },
+            subject: {
+                type: DataTypes.STRING(30),
+                allowNull: false,
+            },
+            quantity: {
+                type: DataTypes.INTEGER(3),
+                allowNull: false,
+            },
+            unit: {
+                type: DataTypes.STRING(10),
+                allowNull: false,
+            },
+            startTime: {
+                type: DataTypes.STRING(10),
+                allowNull: false,
+                defaultValue: 'none',
+            },
+            endTime: {
+                type: DataTypes.STRING(10),
+                allowNull: false,
+                defaultValue: 'none',
+            },
+            allDayStatus: {
+                type: DataTypes.BOOLEAN,
+                allowNull: false,
+                defaultValue: false,
+            },
+            important: {
+                type: DataTypes.BOOLEAN,
+                allowNull: false,
+                defaultValue: false,
+            },
+        }, {
+            modelName: 'Todo',
+            tableName: 'todos',
+            charset: 'utf8mb4',
+            collate: 'utf8mb4_general_ci',
+            sequelize,
+        });
+    }
+    static associate(db) {
+        //db.Todo.belongsTo(db.User);
+    }
+};
