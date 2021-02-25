c14f6df6347415e4a2b8e951b984e21a14e43d25
hyoni0817 <dudu1104@naver.com>
2021-02-25 21:00:17 +0900

    Add: Dday saga

--- front/sagas/index.js
+++ front/sagas/index.js
@@ -2,11 +2,13 @@
 import { all, call } from 'redux-saga/effects';
 import axios from 'axios';
 import todo from './todo';
+import Dday from './dday';
 
 axios.defaults.baseURL = "http://localhost:3065/api";
 
 export default function* rootSaga() {
     yield all([
         call(todo),
+        call(Dday),
     ])
 }
