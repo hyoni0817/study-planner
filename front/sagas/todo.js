0f1f9e30b80d45589048445f209cbfa07c12c633
hyoni0817 <dudu1104@naver.com>
2021-02-25 14:26:16 +0900

    Add: todo 로드 관련 saga

--- front/sagas/todo.js
+++ front/sagas/todo.js
@@ -1,6 +1,6 @@
 import { all, fork, takeLatest, call, put } from 'redux-saga/effects';
 import axios from 'axios';
-import { ADD_TODO_REQUEST, ADD_TODO_SUCCESS, ADD_TODO_FAILURE } from '../reducers/todo';
+import { ADD_TODO_REQUEST, ADD_TODO_SUCCESS, ADD_TODO_FAILURE, LOAD_TODO_LIST_REQUEST, LOAD_TODO_LIST_SUCCESS, LOAD_TODO_LIST_FAILURE } from '../reducers/todo';
 
 function addTodoAPI(todoData) {
     return axios.post('/todo', todoData);
@@ -24,8 +24,34 @@ function* addTodo(action) {
 function* watchAddTodo() {
     yield takeLatest(ADD_TODO_REQUEST, addTodo);
 }
+
+function loadTodoAPI() {
+    return axios.get('/todolist');
+}
+
+function* loadTodo() {
+    try {
+        const result = yield call(loadTodoAPI);
+        yield put({
+            type: LOAD_TODO_LIST_SUCCESS,
+            data: result.data,
+        })
+
+    } catch(e) {
+        console.error(e)
+        yield put({
+            type: LOAD_TODO_LIST_FAILURE,
+            error: e,
+        })
+    }
+}
+
+function* watchLoadTodo() {
+    yield takeLatest(LOAD_TODO_LIST_REQUEST, loadTodo);
+}
 export default function* todoSaga() {
     yield all([
         fork(watchAddTodo),
+        fork(watchLoadTodo),
     ]);
 }
