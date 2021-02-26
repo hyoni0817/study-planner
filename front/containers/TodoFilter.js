11fbd3c5f05839241d68024815505b895c9fd735
hyoni0817 <dudu1104@naver.com>
2021-02-27 02:02:38 +0900

    Refactor: todo 검색 용도에 맞게 전면 수정

--- front/containers/TodoFilter.js
+++ front/containers/TodoFilter.js
@@ -1,41 +1,109 @@
-import React from 'react';
-import { Input, DatePicker, Checkbox, Select } from 'antd';
+import React, { useState } from 'react';
+import { Input, DatePicker, Checkbox, Select, Form, Button } from 'antd';
+import moment from 'moment';
 
 const TodoFilter = () => {
     const { Search } = Input;
     const { RangePicker } = DatePicker;
     const { Option } = Select;
     const children = [];
+    const dateFormat = 'YYYY-MM-DD';
+    const date = new Date();
+    const todayDate = `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`;
 
-    const onSearch = value => console.log(value);
+    const [ todoTitle, setTodoTitle ] = useState('');
+    const [ startDate, setStartDate ] = useState('');
+    const [ endDate, setEndDate ] = useState('');
+    const [ subjects, setSubjects ] = useState([]);
+    const [ allDateCheckState, setllDateCheckState ] = useState(false);
 
-    const onChange = (e) => {
+    const layout = {
+        labelCol: {
+            span: 8,
+        },
+        wrapperCol: {
+            span: 16,
+        }
+    }
+
+    const onChangeSearchTodo = (e) => {
+        setTodoTitle(e.target.value);
+    };
+
+    const onChangeDate = (value) => {
+        if(value) {
+            const startDateFormat = moment(value[0]["_d"]).format(dateFormat);
+            const endDateFormat = moment(value[1]["_d"]).format(dateFormat); 
+            setStartDate(startDateFormat);
+            setEndDate(endDateFormat);
+            startDateFormat && endDateFormat ? setCheckDate(false) : setCheckDate(true);    
+        } else {
+            setStartTime('');
+            setEndTime('');
+            setCheckTime(true);
+        }    
+    }
+
+    function onChangeAllDateCheckBox(e) {
         console.log(`checked = ${e.target.checked}`);
+        setllDateCheckState(e.target.checked);
     }
 
     for (let i = 10; i < 36; i++) {
         children.push(<Option key={i.toString(36) + i}>{i.toString(36) + i}</Option>);
     }
 
-    const handleChange = (value) => {
+    const onChangeSubjects = (value) => {
+        setSubjects(value);
         console.log(`selected ${value}`);
     }
 
+    const onFinish = (values) => {
+
+    };
+
     return (
         <>
-            <Search placeholder="input search text" onSearch={onSearch} enterButton />
-            <RangePicker />
-            <Checkbox onChange={onChange}>완료한 할 일 포함</Checkbox>
-            <Select
-                mode="multiple"
-                allowClear
-                style={{ width: '100%' }}
-                placeholder="Please select"
-                defaultValue={['a10', 'c12']}
-                onChange={handleChange}
+            <Form
+                {...layout}
+                name="searchForm"
+                onFinish={onFinish}
             >
-                {children}
-            </Select>
+                <Form.Item
+                    name="todoTitle"
+                >
+                    <Input placeholder="계획명을 입력해주세요" onChange={onChangeSearchTodo} />
+                </Form.Item>
+                <Form.Item
+                    name="date"
+                >
+                    <RangePicker 
+                        defaultValue={[moment(todayDate, dateFormat), moment(todayDate, dateFormat)]}
+                        format={dateFormat}
+                        onChange={onChangeDate}
+                        disabled={allDateCheckState}
+                    />      
+                    <Checkbox onChange={onChangeAllDateCheckBox} style={{ marginLeft: '10px', }}>전체</Checkbox>
+                </Form.Item>
+                <Form.Item
+                    name="subjects"
+                >
+                    <Select
+                        mode="multiple"
+                        allowClear
+                        style={{ width: '100%' }}
+                        placeholder="검색할 과목을 입력해주세요"
+                        onChange={onChangeSubjects}
+                    >
+                        {children}
+                    </Select>
+                </Form.Item>
+                <Form.Item>
+                    <Button type="primary" htmlType="submit">
+                        검색
+                    </Button>
+                </Form.Item>
+            </Form>
         </>
     );
 };
