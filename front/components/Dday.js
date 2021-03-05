56c24c525f6035f92589a980b554afae0c4bec5d
hyoni0817 <dudu1104@naver.com>
2021-03-06 00:31:31 +0900

    Move: Dday 컴포넌트 재사용성을 높이기 위해 일부 코드 이동
    - UI 관련 컴포넌트(Row, Col), Redux 관련 코드 이동시킴.
    - 코드를 이동시킴으로써 페이지에 따라 Dday 컴포넌트를 커스텀하기 쉬워짐.

--- front/components/Dday.js
+++ front/components/Dday.js
@@ -1,9 +1,9 @@
-import React, { useEffect } from 'react';
-import { Col, Row } from 'antd';
-import { useSelector, useDispatch } from 'react-redux';
+import React from 'react';
+import { Col } from 'antd';
+import { useSelector } from 'react-redux';
 import styled from 'styled-components';
+import { LOAD_TODO_LIST_REQUEST } from '../reducers/todo';
 
-import { LOAD_DDAY_LIST_REQUEST } from '../reducers/dday';
 const DdayBox = styled.div`
     background: linear-gradient(to right, #3a1c71, #d76d77, #ffaf7b);
     border-radius: 5px;
@@ -30,15 +30,8 @@ const DdayDate = styled.p`
     text-align: right;
     margin-bottom: 7px;
 `
-const Dday = () => {
-    const dispatch = useDispatch();
-    const { DdayList } = useSelector( state => state.dday )
+const Dday = ({data}) => {
 
-    useEffect(() => {
-        dispatch({
-            type: LOAD_DDAY_LIST_REQUEST,
-        });
-    }, []);
     const calculateDday = (dueDate) => {
         const dateArr = dueDate.split('-');
         const now = new Date();
@@ -51,24 +44,11 @@ const Dday = () => {
 
     return (
         <>
-            <div style={{ padding: '15px 15px 15px 0', textAlign: 'center', overflowX: 'auto', }}>
-                <Row gutter={16} justify="center" style={{ marginRight: '0', }}>
-                    { 
-                        DdayList.length == 0 ? <p style={{textAlign: 'center'}}>아직 D-day가 등록되지 않았습니다.</p> 
-                        : DdayList.map((c) => {
-                            return (
-                                <Col xs={12} sm={6} md={10} lg={6} >
-                                    <DdayBox bordered={false}>
-                                        <DdayDate>{c.dueDate} 까지</DdayDate>
-                                        <DdayTitle>{c.title}</DdayTitle>
-                                        <DdayContent>D{ calculateDday(c.dueDate) }</DdayContent>
-                                    </DdayBox> 
-                                </Col>
-                            )
-                        })  
-                    }                    
-                </Row>
-            </div> 
+            <DdayBox bordered={false}>
+                <DdayDate>{data.dueDate} 까지</DdayDate>
+                <DdayTitle>{data.title}</DdayTitle>
+                <DdayContent>D{ calculateDday(data.dueDate) }</DdayContent>
+            </DdayBox> 
         </>
     )
 };
--- front/pages/index.js
+++ front/pages/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Progress, Button, Affix } from 'antd';
+import { Progress, Button, Affix, Row, Col } from 'antd';
 import Todo from '../components/Todo';
 import Dday from '../components/Dday';
 import SelectForms from '../components/SelectForms';
@@ -9,6 +9,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import styled from 'styled-components';
 
 import { LOAD_TODO_LIST_REQUEST } from '../reducers/todo';
+import { LOAD_DDAY_LIST_REQUEST } from '../reducers/dday';
 
 const TodoNowWrapper = styled.div`
     margin-bottom: 30px;
@@ -39,6 +40,8 @@ const Home = (props) => {
     const router = useRouter();
     const dispatch = useDispatch();
     const { todoList } = useSelector( state => state.todo )
+    const { DdayList } = useSelector( state => state.dday )
+
     const tempTodo = {title: "수학문제 풀기", quantity: 11, unit: "개", important: false, selectSubject: "수학", startTime: "9:00", endTime: "10:00", allDayStatus: false, }
     
     const date = new Date();
@@ -47,7 +50,10 @@ const Home = (props) => {
     useEffect(() => {
         dispatch({
             type: LOAD_TODO_LIST_REQUEST,
-        })
+        });
+        dispatch({
+            type: LOAD_DDAY_LIST_REQUEST,
+        });
     }, []);
     const onClickWriteBtn = () => {
         router.push('/createplan');
@@ -55,7 +61,21 @@ const Home = (props) => {
     return (
         <>
             <div>{date.getFullYear()}년 {date.getMonth()+1}월 {date.getDate()}일 {days[date.getDay()]}요일</div>
-            <Dday />
+            <div style={{ padding: '15px 15px 15px 0', textAlign: 'center', overflowX: 'auto', }}>
+                <Row gutter={16} justify="center" style={{ marginRight: '0', }}>
+                    { 
+                        DdayList.length == 0 ? <p style={{textAlign: 'center'}}>아직 D-day가 등록되지 않았습니다.</p> 
+                        : DdayList.map((c) => {
+                            console.log("c:", c)
+                            return (
+                                <Col xs={12} sm={6} md={10} lg={6}>
+                                    <Dday key={c.id} data={c} />    
+                                </Col>
+                            )
+                        })  
+                    }                    
+                </Row>
+            </div> 
             <div>
                 <p>오늘의 성취율</p>
                 <Progress
