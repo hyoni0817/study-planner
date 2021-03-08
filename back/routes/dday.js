48a05d9d8b9fba70e93c4f3e920a010c3598f0fc
hyoni0817 <dudu1104@naver.com>
2021-03-08 17:15:09 +0900

    Add: Dday 검색 API

--- back/routes/dday.js
+++ back/routes/dday.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const Sequelize = require('sequelize');
+const { Op } = require('sequelize');
 
 const router = express.Router();
 const db = require('../models');
@@ -15,6 +17,30 @@ router.post('/', async (req, res, next) => {
         console.error(e);
         return next(e);
     }
+});
+
+router.get('/search', async (req, res, next) => {
+    try {
+        let where = [Sequelize.literal(`MATCH (title) AGAINST ('+${req.query.DdayTitle}*' in boolean mode)`)];
+        const dueDate = req.query.allDateCheckState === true ? '' : where.push({dueDate: {
+            [Op.and]: {
+                [Op.gte] : req.query.startDate,
+                [Op.lte] : req.query.endDate
+            }
+        }});
+        const memo = req.query.memo.length === 0 ? '' :  where.push(
+            Sequelize.literal(`MATCH (memo) AGAINST ('+${req.query.memo}*' in boolean mode)`)
+        )
+        const searchCondition = await db.Dday.findAll({
+            where,
+            attributes: ['id', 'title', 'dueDate', 'memo'], 
+        });
+
+        return res.json(searchCondition);
+    } catch (e) {
+        console.error(e);
+        return next(e);
+    } 
 });
 
 module.exports = router;
