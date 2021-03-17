8ef33bda54db89ef0a5e5e987d2208e35cf890c3
hyoni0817 <dudu1104@naver.com>
2021-03-17 19:45:58 +0900

    Add: '홈 화면에서 보이기'를 선택한 Dday 리스트 로드하는 API - 무한 스크롤링도 추가로 같이 구현함.

--- back/routes/ddayList.js
+++ back/routes/ddayList.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const Sequelize = require('sequelize');
+const { Op } = require('sequelize');
 
 const router = express.Router();
 const db = require('../models');
@@ -15,5 +17,29 @@ router.get('/', async (req, res, next) => {
         return next(e);
     }
 });
+
+router.get('/viewable', async(req, res, next) => {
+    try {
+        let where = [{viewState : 1}];
+        if (parseInt(req.query.lastId)) {
+            where.push({ 
+                id: {
+                    [Op.gt]: parseInt(req.query.lastId, 10), 
+                }
+            })
+        } 
+
+        const viewableList = await db.Dday.findAll({
+            where,
+            attributes: ['id', 'title', 'memo', 'dueDate', 'viewState'],
+            limit: parseInt(req.query.limit),
+        });
+        res.json(viewableList);
+    } catch (e) {
+        console.error(e);
+        return next(e);
+    }
+
+})
 
 module.exports = router;
