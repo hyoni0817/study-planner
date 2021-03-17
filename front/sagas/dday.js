b1dff2025d9d66f43b4e5336dc399398ce87360e
hyoni0817 <dudu1104@naver.com>
2021-03-17 19:44:13 +0900

    Add: '홈 화면에서 보이기'를 선택한 Dday 리스트 로드 관련 saga - 무한 스크롤링도 추가로 같이 구현함.

--- front/sagas/dday.js
+++ front/sagas/dday.js
@@ -1,6 +1,6 @@
 import { all, fork, takeLatest, call, put, select } from 'redux-saga/effects';
 import axios from 'axios';
-import { ADD_DDAY_REQUEST, ADD_DDAY_SUCCESS, ADD_DDAY_FAILURE, LOAD_DDAY_LIST_REQUEST, LOAD_DDAY_LIST_SUCCESS, LOAD_DDAY_LIST_FAILURE, SEARCH_DDAY_LIST_REQUEST, SEARCH_DDAY_LIST_SUCCESS, SEARCH_DDAY_LIST_FAILURE, EDIT_DDAY_REQUEST, EDIT_DDAY_SUCCESS, EDIT_DDAY_FAILURE, DELETE_DDAY_REQUEST, DELETE_DDAY_SUCCESS, DELETE_DDAY_FAILURE, SHOW_DDAY_REQUEST, SHOW_DDAY_SUCCESS, SHOW_DDAY_FAILURE } from '../reducers/dday';
+import { ADD_DDAY_REQUEST, ADD_DDAY_SUCCESS, ADD_DDAY_FAILURE, LOAD_DDAY_LIST_REQUEST, LOAD_DDAY_LIST_SUCCESS, LOAD_DDAY_LIST_FAILURE, SEARCH_DDAY_LIST_REQUEST, SEARCH_DDAY_LIST_SUCCESS, SEARCH_DDAY_LIST_FAILURE, EDIT_DDAY_REQUEST, EDIT_DDAY_SUCCESS, EDIT_DDAY_FAILURE, DELETE_DDAY_REQUEST, DELETE_DDAY_SUCCESS, DELETE_DDAY_FAILURE, SHOW_DDAY_REQUEST, SHOW_DDAY_SUCCESS, SHOW_DDAY_FAILURE, LOAD_VIEWABLE_DDAY_LIST_REQUEST, LOAD_VIEWABLE_DDAY_LIST_SUCCESS, LOAD_VIEWABLE_DDAY_LIST_FAILURE } from '../reducers/dday';
 
 function addDdayAPI(DdayData) {
     return axios.post('/dday', DdayData);
@@ -55,9 +55,33 @@ function* watchLoadDday() {
     yield takeLatest(LOAD_DDAY_LIST_REQUEST, loadDday);
 }
 
-function searchDdayAPI(conditionData) {
-    console.log("conditionData:", conditionData);
-    return axios.get(`/dday/search?DdayTitle=${conditionData.DdayTitle}&allDateCheckState=${conditionData.allDateCheckState}&startDate=${conditionData.startDate}&endDate=${conditionData.endDate}&memo=${conditionData.memo}`);
+function loadViewableDdayAPI(lastId=0, limit=10) {
+    return axios.get(`/ddaylist/viewable?lastId=${lastId}&limit=${limit}`);
+}
+
+function* loadViewableDday(action) {
+    try {
+        const result = yield call(loadViewableDdayAPI, action.lastId);
+        yield put({
+            type: LOAD_VIEWABLE_DDAY_LIST_SUCCESS,
+            data: result.data,
+        })
+
+    } catch(e) {
+        console.error(e)
+        yield put({
+            type: LOAD_VIEWABLE_DDAY_LIST_FAILURE,
+            error: e,
+        })
+    }
+}
+
+function* watchLoadViewableDday() {
+    yield takeLatest(LOAD_VIEWABLE_DDAY_LIST_REQUEST, loadViewableDday);
+}
+
+function searchDdayAPI(conditionData, lastId=0, limit=10) {
+    return axios.get(`/dday/search?DdayTitle=${conditionData.DdayTitle}&allDateCheckState=${conditionData.allDateCheckState}&startDate=${conditionData.startDate}&endDate=${conditionData.endDate}&memo=${conditionData.memo}&lastId=${lastId}&limit=${limit}`);
 }
 
 function* searchDday(action) {
@@ -154,6 +178,7 @@ export default function* DdaySaga() {
     yield all([
         fork(watchAddDday),
         fork(watchLoadDday),
+        fork(watchLoadViewableDday),
         fork(watchSearchDday),
         fork(watchEditDday),
         fork(watchDeleteDday),
