584aa75c192a16ad91f9d0aa48c9fa43f4296b2e
hyoni0817 <dudu1104@naver.com>
2021-04-13 23:50:06 +0900

    Add: user saga

--- front/sagas/index.js
+++ front/sagas/index.js
@@ -3,6 +3,7 @@ import { all, call } from 'redux-saga/effects';
 import axios from 'axios';
 import todo from './todo';
 import Dday from './dday';
+import user from './user';
 
 axios.defaults.baseURL = "http://localhost:3065/api";
 
@@ -10,5 +11,6 @@ export default function* rootSaga() {
     yield all([
         call(todo),
         call(Dday),
+        call(user),
     ])
 }
