ad27a6cf545420dca515da2562e409c322a9d892
hyoni0817 <dudu1104@naver.com>
2021-04-18 19:43:03 +0900

    Rename: add user를 sign up으로 이름 변경

--- front/sagas/user.js
+++ front/sagas/user.js
@@ -1,34 +1,34 @@
 import { all, call, put, takeLatest } from 'redux-saga/effects';
 import axios from 'axios';
-import { ADD_USER_REQUEST, ADD_USER_SUCCESS, ADD_USER_FAILURE } from '../reducers/user';
+import { SIGN_UP_REQUEST, SIGN_UP_SUCCESS, SIGN_UP_FAILURE } from '../reducers/user';
 
-function addUserAPI(userData) {
+function signUpAPI(userData) {
     return axios.post('/user', userData);
 }
 
-function* addUser(action) {
+function* signUp(action) {
     console.log("action.data:", action.data);
     try {
-        const result = yield call(addUserAPI, action.data);
+        const result = yield call(signUpAPI, action.data);
         yield put({
-            type: ADD_USER_SUCCESS,
+            type: SIGN_UP_SUCCESS,
             data: result.data,
         })
     } catch (e) {
         console.error(e);
         yield put({
-            type: ADD_USER_FAILURE,
+            type: SIGN_UP_FAILURE,
             error: e,
         })
     }
 }
 
-function* watchAddUser() {
-    yield takeLatest(ADD_USER_REQUEST, addUser);
+function* watchSignUp() {
+    yield takeLatest(SIGN_UP_REQUEST, signUp);
 }
 
 export default function* userSaga() {
     yield all([
-        fork(watchAddUser),
+        fork(watchSignUp),
     ])
 }
