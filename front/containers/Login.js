fc7245f8c44ac9a57df2d67c66eaffc76cdc35fc
hyoni0817 <dudu1104@naver.com>
2021-04-19 22:58:39 +0900

    Refactor: Form 중앙 정렬 및 라벨 제거, Modal 크기 등을 수정
    - Ant Design의 Row와 Col 을 사용해 중앙 정렬함.
    
    - 라벨 대신 placeholder로 대체함.
    
    - 라벨을 제거함에 따라 layout 및 tailLayout 제거함.

--- front/containers/Login.js
+++ front/containers/Login.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { Form, Input, Button, Checkbox, Modal } from 'antd';
+import { Form, Input, Button, Checkbox, Modal, Row, Col } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
 
 //redux
 //import {} from '../reducers/user';
@@ -34,49 +35,51 @@ const Login = ({isOpen}) => {
                 onCancel={onHandleCancel}
                 okButtonProps={{ disabled: true }}
                 cancelButtonProps={{ disabled: true }}
+                width={400}
             >
-                <Form
-                    {...layout}
-                    initialValues={{
-                        remember: true,
-                    }}
-                    onFinish={onFinish}
-                >
-                    <Form.Item
-                        label="ID"
-                        name="userId"
-                        rules={[
-                        {
-                            required: true,
-                            message: '아이디를 입력해주세요.',
-                        },
-                        ]}
-                    >
-                        <Input />
-                    </Form.Item>
+                <Row type="flex" justify="center" align="middle" style={{margin: '30px 0'}}>
+                    <Col>
+                        <Form
+                            initialValues={{
+                                remember: true,
+                            }}
+                            onFinish={onFinish}
+                        >
+                            <Form.Item
+                                name="userId"
+                                rules={[
+                                {
+                                    required: true,
+                                    message: '아이디를 입력해주세요.',
+                                },
+                                ]}
+                            >
+                                <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="아이디"/>
+                            </Form.Item>
 
-                    <Form.Item
-                        label="Password"
-                        name="password"
-                        rules={[
-                        {
-                            required: true,
-                            message: '비밀 번호를 입력해주세요.',
-                        },
-                        ]}
-                    >
-                        <Input.Password />
-                    </Form.Item>
-                    <Form.Item {...tailLayout} name="remember" valuePropName="checked">
-                        <Checkbox>Remember me</Checkbox>
-                    </Form.Item>
+                            <Form.Item
+                                name="password"
+                                rules={[
+                                {
+                                    required: true,
+                                    message: '비밀 번호를 입력해주세요.',
+                                },
+                                ]}
+                            >
+                                <Input.Password prefix={<LockOutlined className="site-form-item-icon" />} placeholder="비밀 번호"/>
+                            </Form.Item>
+                            <Form.Item name="remember" valuePropName="checked">
+                                <Checkbox>로그인 상태 유지</Checkbox>
+                            </Form.Item>
 
-                    <Form.Item {...tailLayout}>
-                        <Button type="primary" htmlType="submit">
-                        Submit
-                        </Button>
-                    </Form.Item>
-                </Form>
+                            <Form.Item>
+                                <Button type="primary" htmlType="submit" style={{width: '100%'}}>
+                                    로그인 하기
+                                </Button>
+                            </Form.Item>
+                        </Form>
+                    </Col>
+                </Row>
             </Modal>
         </>
     )
