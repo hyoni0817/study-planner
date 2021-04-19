baebe84c3aa11a42be3059641928f1f7c7f9123d
hyoni0817 <dudu1104@naver.com>
2021-04-20 01:30:37 +0900

    Rename&Move: Login 파일명 변경 및 로그인 폼 부분 코드 분리
    - 데스크탑 버전에서만 Modal창에 로그인 폼을 보여주게 되어 Login에서 DesktopLogin으로 변경함.
    
    - 모바일 버전에서도 로그인 폼을 공통으로 사용하게 되어 LoginForm 컴포넌트로 따로 분리시킴.

---
+++ front/containers/DesktopLogin.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import { Modal } from 'antd';
+import LoginForm from '../containers/LoginForm';
+
+const DesktopLogin = ({isOpen}) => {
+    const [visible, setVisible] = useState(true);
+ 
+    const onHandleCancel = () => {
+        setVisible(false);
+        isOpen(false);
+    };
+
+    return (
+        <>
+            <Modal
+                visible={visible}
+                footer={null}
+                onCancel={onHandleCancel}
+                okButtonProps={{ disabled: true }}
+                cancelButtonProps={{ disabled: true }}
+                width={400}
+            >
+                <LoginForm onSubmit={onHandleCancel}/>
+            </Modal>
+        </>
+    )
+}
+
+export default DesktopLogin;
---
+++ front/containers/LoginForm.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Form, Input, Button, Checkbox, Modal, Row, Col } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+
+//redux
+//import {} from '../reducers/user';
+
+const LoginForm = ({onSubmit}) => {
+
+    const onFinish = (values) => {
+        console.log('Success:', values);
+        onSubmit();
+    };
+
+    return (
+        <>
+            <Row type="flex" justify="center" align="middle" style={{margin: '30px 0'}}>
+                <Col>
+                    <Form
+                        initialValues={{
+                            remember: true,
+                        }}
+                        onFinish={onFinish}
+                    >
+                        <Form.Item
+                            name="userId"
+                            rules={[
+                            {
+                                required: true,
+                                message: '아이디를 입력해주세요.',
+                            },
+                            ]}
+                        >
+                            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="아이디"/>
+                        </Form.Item>
+
+                        <Form.Item
+                            name="password"
+                            rules={[
+                            {
+                                required: true,
+                                message: '비밀 번호를 입력해주세요.',
+                            },
+                            ]}
+                        >
+                            <Input.Password prefix={<LockOutlined className="site-form-item-icon" />} placeholder="비밀 번호"/>
+                        </Form.Item>
+                        <Form.Item name="remember" valuePropName="checked">
+                            <Checkbox>로그인 상태 유지</Checkbox>
+                        </Form.Item>
+
+                        <Form.Item>
+                            <Button type="primary" htmlType="submit" style={{width: '100%'}}>
+                                로그인 하기
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                </Col>
+            </Row>
+        </>
+    )
+}
+
+export default LoginForm;
