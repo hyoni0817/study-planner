bdc233266e796e5f18d25a5ba0e6a339fb14b0fc
hyoni0817 <dudu1104@naver.com>
2021-04-21 02:43:52 +0900

    Add: Portal을 사용하여 모바일 버전 로그인 화면 구현

---
+++ front/containers/MobileLogin.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import styled from 'styled-components';
+import LoginForm from './LoginForm';
+import { CloseOutlined } from '@ant-design/icons';
+
+const LoginFormWrapper = styled.div`
+    background-color: white;
+    padding: 24px;       
+    position: fixed;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    overflow:auto;
+    z-index:205;
+    text-align: center;
+`;
+
+const CloseBtn = styled.button`
+    background-color: #ffffff00;
+    border: none;
+    font-size: 20px;
+    cursor: pointer;
+`
+
+const MobileLogin = ({isOpen}) => {
+    const onHandleClose = () => {
+        isOpen(false);
+    }
+
+    return (
+        <>  
+            <LoginFormWrapper>
+                <div style={{textAlign: 'right'}}>
+                    <CloseBtn onClick={onHandleClose}><CloseOutlined /></CloseBtn>
+                </div>
+                <h1>Study Planner</h1>
+                <LoginForm onSubmit={onHandleClose} />
+            </LoginFormWrapper>
+        </>
+    );
+}
+
+export default MobileLogin;
--- front/pages/_document.js
+++ front/pages/_document.js
@@ -38,6 +38,7 @@ class MyDocument extends Document {
 
                     {/* Portal을 사용한 모바일 폼을 Main 컴포넌트 위에 겹쳐서 보여주기 위해 div 추가  */}
                     <div id="mobile-form"></div>
+                    <div id="mobile-login"></div>
 
                     {/* ie 에서의 실행을 위해 폴리필 사용 */}
                     {process.env.NODE_ENV === 'production'
