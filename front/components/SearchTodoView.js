f281a7442106200bf2ed116e62e8413bfc62c6af
hyoni0817 <dudu1104@naver.com>
2021-04-28 23:58:45 +0900

    Add: todo 보여주는 부분 디자인 재구성 및 검색시 todo 보여주는 부분 따로 구현

---
+++ front/components/SearchTodoView.js
@@ -0,0 +1,126 @@
+import React, { useCallback } from 'react';
+import { Tag, Row, Col } from 'antd';
+import { ExclamationCircleOutlined, CheckOutlined } from '@ant-design/icons';
+import styled from 'styled-components';
+
+//redux
+import { useDispatch } from 'react-redux'
+import { COMPLETE_TODO_REQUEST } from '../reducers/todo';
+import EditFormButton from './EditFormButton';
+import DeleteButton from '../containers/DeleteButton';
+
+const TodoRow = styled(Row)`
+  margin-bottom: 20px;
+  background: #F6BD16;
+  border-radius: 5px;
+  color: #ffffffe8;
+`
+const TodoBox = styled.tr`
+  margin-bottom: 20px;
+  width: 100%;  
+  background: #F6BD16;
+`;
+
+const TodoTimeCell = styled.div`
+  padding-right: 10px;
+  vertical-align: middle;
+  text-align: right;
+  width: 100px;
+`;
+
+const TodoDetailCell = styled.div`
+  margin-top: 10px;
+  text-align: left;
+`;
+
+const TodoCompletionStatusCell = styled.p`
+  vertical-align: middle;
+  text-align: left;
+  width: 35px;
+`;
+
+const TodoEditAndDeleteCell = styled.div`
+  text-align: right;
+`;
+
+const TodoImportantStatus = styled.p`
+  color: red;
+  margin-bottom: 0;
+`;
+
+const TodoTitle = styled.p`
+  font-weight: 600; 
+  font-size: 15px;
+  margin-bottom: 0;
+  margin-top: 10px;
+`;
+
+const SearchTodoView = ({data}) => {
+    const dispatch = useDispatch();
+    const completed = data.completion;
+
+    const CircleCheckBtn = styled.button`
+      color: #F6BD16;
+      height: 28px;
+      width: 28px;
+      background-color: ${completed ? 'white' : '#ffffffe8'};
+      border-radius: 50%;
+      display: block;
+      cursor:pointer;
+      border: 2px solid #F6BD16;
+      margin: 10px;
+
+      &: focus {
+        outline: none;
+      }
+    `;
+
+    const onClickCheckBtn = useCallback(() => {
+      if(!completed) {
+        //완료
+        dispatch({
+          type: COMPLETE_TODO_REQUEST,
+          data: {
+            id: data.id,
+            checkBtnState: true,
+          }
+        })
+      } else {
+        //완료 취소
+        dispatch({
+          type: COMPLETE_TODO_REQUEST,
+          data: {
+            id: data.id,
+            checkBtnState: false,
+          }
+        })
+      }
+    }, [data && data.id, completed])
+
+    return (
+        <>
+            <TodoRow>
+              <Col span={2}>
+                <CircleCheckBtn onClick={onClickCheckBtn}>{ completed ? <CheckOutlined /> : '' }</CircleCheckBtn>
+              </Col>
+              <Col span={22}>
+                <TodoDetailCell>
+                
+                  {data.important ? <TodoImportantStatus><ExclamationCircleOutlined />중요</TodoImportantStatus> : ''}
+                  {data.allDayStatus ? '하루 종일' : `${data.startTime} - ${data.endTime}`}
+                
+                  <TodoTitle><Tag color="magenta">{data.subject}</Tag>{data.title}</TodoTitle>
+                  분량은 {data.quantity} {data.unit}!
+                </TodoDetailCell>
+                <TodoEditAndDeleteCell>
+                  <EditFormButton data={data} type="todo" />
+                  <DeleteButton id={data.id} type="todo" />
+                </TodoEditAndDeleteCell>
+              </Col>
+            </TodoRow>     
+            
+        </>
+    );
+};
+
+export default SearchTodoView;
--- front/components/Todo.js
+++ front/components/Todo.js
@@ -2,6 +2,7 @@ import React, { useCallback } from 'react';
 import { Tag } from 'antd';
 import { ExclamationCircleOutlined, CheckOutlined } from '@ant-design/icons';
 import styled from 'styled-components';
+import SearchTodoView from './SearchTodoView';
 
 //redux
 import { useDispatch } from 'react-redux'
@@ -9,38 +10,44 @@ import { COMPLETE_TODO_REQUEST } from '../reducers/todo';
 import EditFormButton from './EditFormButton';
 import DeleteButton from '../containers/DeleteButton';
 
-const TodoBox = styled.tr`
-  margin-bottom: 20px;
-  width: 100%;
-`;
 
-const TodoTimeCell = styled.td`
+
+const TodoTime = styled.p`
   padding-right: 10px;
-  border-right: 2px solid black; 
-  vertical-align: middle;
-  text-align: right;
-  width: 100px;
+  //vertical-align: middle;
+  text-align: left;
+  margin-bottom: 0;
 `;
 
-const TodoDetailCell = styled.td`
+const TodoDetailCell = styled.div`
   text-align: left;
+  
 `;
 
 const TodoCompletionStatusCell = styled.td`
   vertical-align: middle;
-  text-align: left;
-  width: 35px;
+  text-align: center;
+  width: 50px;
 `;
 
 const TodoEditAndDeleteCell = styled.td`
-
+ text-align: right;
 `;
 
 const TodoImportantStatus = styled.p`
+  color: red;
   margin-bottom: 0;
 `;
 
+
 const TodoTitle = styled.p`
+  font-size: 15px;
+  margin-bottom: 0;
+  overflow:hidden; 
+  text-overflow:ellipsis; 
+  white-space:nowrap;
+  font-weight: 600; 
+  margin-top: 5px;
   margin-bottom: 0;
 `;
 
@@ -48,14 +55,36 @@ const Todo = ({post, view}) => {
     const dispatch = useDispatch();
     const completed = post.completion;
 
+    const TodoBoxTable = styled.table`
+      border-radius: 5px;
+      margin: 1.5% 1.5%;
+      width: ${ view == `search` || view == `now` ? `100%` : `47%`};
+      height: ${ view == `now` ? `70px` : `100px`};
+      color: ${ view == `now` ? `black` : `#ffffffe8`};
+      float: left;
+      ${ view == `now` ? `` : `background: #F6BD16;`}
+      table-layout: fixed;
+      @media screen and (max-width:767px) {
+        width:100%;
+        margin: 1.5% 0;
+      }
+    }
+    `;
+
     const CircleCheckBtn = styled.button`
-      height: 25px;
-      width: 25px;
-      background-color: ${completed ? 'red' : '#bbb'};
+      color: #F6BD16;
+      height: 28px;
+      width: 28px;
+      background-color: ${completed ? 'white' : '#ffffffe8'};
       border-radius: 50%;
-      display: inline-block;
+      display: block;
       cursor:pointer;
-      border: none;
+      border: 2px solid #F6BD16;
+      margin: 10px;
+
+      &: focus {
+        outline: none;
+      }
     `;
 
     const onClickCheckBtn = useCallback(() => {
@@ -83,26 +112,25 @@ const Todo = ({post, view}) => {
     return (
         <>
           {
-            post === undefined ? '' :
-            <TodoBox>
-              <TodoCompletionStatusCell>
-                <CircleCheckBtn onClick={onClickCheckBtn}>{ completed ? <CheckOutlined /> : '' }</CircleCheckBtn>
-              </TodoCompletionStatusCell>
-              <TodoTimeCell>
-                {post.important ? <TodoImportantStatus><ExclamationCircleOutlined />중요</TodoImportantStatus> : ''}
-                {post.allDayStatus ? '하루 종일' : `${post.startTime} - ${post.endTime}`}
-              </TodoTimeCell>
-              <TodoDetailCell>
-                <TodoTitle><Tag color="magenta">{post.subject}</Tag>{post.title}</TodoTitle>
-                0 / {post.quantity} {post.unit}
-              </TodoDetailCell>
-              { view == 'search' ? 
-              <TodoEditAndDeleteCell>
-                <EditFormButton data={post} type="todo" />
-                <DeleteButton id={post.id} type="todo" />
-              </TodoEditAndDeleteCell>
-              : '' }
-            </TodoBox>     
+            post === undefined ? '' : view !== 'search' ?
+            <TodoBoxTable>
+              <tr>
+                <TodoCompletionStatusCell>
+                  <CircleCheckBtn onClick={onClickCheckBtn}>{ completed ? <CheckOutlined /> : '' }</CircleCheckBtn>
+                </TodoCompletionStatusCell>
+                <td>
+                  <TodoDetailCell>
+                    <TodoTime>
+                      {post.important ? <TodoImportantStatus><ExclamationCircleOutlined />중요</TodoImportantStatus> : ''}
+                      {view === 'now' ? '' : post.allDayStatus ? '하루 종일' : `${post.startTime} - ${post.endTime}`}
+                    </TodoTime>
+                    <TodoTitle><Tag color="magenta">{post.subject}</Tag>{post.title}</TodoTitle>
+                    분량은 {post.quantity} {post.unit}!
+                  </TodoDetailCell>
+                </td>
+              </tr>
+            </TodoBoxTable> :
+            <SearchTodoView data={post} />
             }  
         </>
     );
