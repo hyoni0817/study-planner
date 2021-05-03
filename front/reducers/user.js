b08ba5f1ad36f2ded14d9ba108cc9661d6e064ee
hyoni0817 <dudu1104@naver.com>
2021-05-03 23:48:55 +0900

    Add: 아이디 중복 체크 관련 액션 및 리듀서

--- front/reducers/user.js
+++ front/reducers/user.js
@@ -1,6 +1,10 @@
 export const initialState = {
     isSignedUp: false,
     isSigningUp: false,
+    isUserIdChecked: false,
+    isUserIdChecking: false,
+    existingUserId: false,
+    userIdCheckReason: '',
     signUpErrorReason: '',
 };
 
@@ -8,6 +12,11 @@ export const initialState = {
 export const SIGN_UP_REQUEST = 'SIGN_UP_REQUEST';
 export const SIGN_UP_SUCCESS = 'SIGN_UP_SUCCESS';
 export const SIGN_UP_FAILURE = 'SIGN_UP_FAILURE';
+
+//아이디 중복 체크하는 액션
+export const USER_ID_CHECK_REQUEST = 'USER_ID_CHECK_REQUEST';
+export const USER_ID_CHECK_SUCCESS = 'USER_ID_CHECK_SUCCESS';
+export const USER_ID_CHECK_FAILURE = 'USER_ID_CHECK_FAILURE';
 
 const reducer = ( state = initialState, action ) => {
     switch (action.type) {
@@ -30,6 +39,25 @@ const reducer = ( state = initialState, action ) => {
                 isSigningUp: false,
                 signUpErrorReason: action.error,
             }
+        case USER_ID_CHECK_REQUEST: 
+            return {
+                ...state,
+                isUserIdChecking: true,
+                isUserIdChecked: false,
+            };
+        case USER_ID_CHECK_SUCCESS: 
+            return {
+                ...state,
+                isUserIdChecking: false,
+                isUserIdChecked: true,
+                existingUserId: action.data,
+            };  
+        case USER_ID_CHECK_FAILURE: 
+            return {
+                ...state,
+                isUserIdChecking: false,
+                userIdCheckReason: action.error,
+            };
         default: {
             return {
                 ...state,
