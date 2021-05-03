5c7a7b1a7bfd9c867d7615f33d6df39ced0f26c8
hyoni0817 <dudu1104@naver.com>
2021-05-03 23:49:43 +0900

    Add: 아이디 중복 체크 관련 saga

--- front/sagas/user.js
+++ front/sagas/user.js
@@ -1,6 +1,6 @@
 import { all, fork, call, put, takeLatest } from 'redux-saga/effects';
 import axios from 'axios';
-import { SIGN_UP_REQUEST, SIGN_UP_SUCCESS, SIGN_UP_FAILURE } from '../reducers/user';
+import { SIGN_UP_REQUEST, SIGN_UP_SUCCESS, SIGN_UP_FAILURE, USER_ID_CHECK_REQUEST, USER_ID_CHECK_SUCCESS, USER_ID_CHECK_FAILURE } from '../reducers/user';
 
 function signUpAPI(userData) {
     return axios.post('/user', userData);
@@ -27,8 +27,34 @@ function* watchSignUp() {
     yield takeLatest(SIGN_UP_REQUEST, signUp);
 }
 
+function userIdCheckAPI(userData) {
+    return axios.post('/user/idcheck', userData);
+}
+
+function* userIdCheck(action) {
+    console.log("action.data:", action.data);
+    try {
+        const result = yield call(userIdCheckAPI, action.data);
+        yield put({
+            type: USER_ID_CHECK_SUCCESS,
+            data: result.data.result,
+        })
+    } catch (e) {
+        console.error(e);
+        yield put({
+            type: USER_ID_CHECK_FAILURE,
+            error: e,
+        })
+    }
+}
+
+function* watchUserIdCheck() {
+    yield takeLatest(USER_ID_CHECK_REQUEST, userIdCheck);
+}
+
 export default function* userSaga() {
     yield all([
         fork(watchSignUp),
+        fork(watchUserIdCheck),
     ])
 }
