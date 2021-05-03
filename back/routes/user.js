f3a835499fd026fc6f8df75f9179ead0fc9392fa
hyoni0817 <dudu1104@naver.com>
2021-05-03 23:52:05 +0900

    Add: 아이디 중복 체크 API

--- back/routes/user.js
+++ back/routes/user.js
@@ -3,6 +3,19 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models');
 
+router.post('/idcheck', async (req, res, next) => {
+    try{
+        const existingId = await db.User.findOne({
+            where: {
+                userId: req.body.userId,
+            },
+        });
+        return existingId ? res.status(200).json({result: true}) : res.status(200).json({result: false});
+    } catch (e) {
+        console.error(e);
+    }
+});
+
 router.post('/', async (req, res, next) => {
     try {
         const newUser = await db.User.create({
