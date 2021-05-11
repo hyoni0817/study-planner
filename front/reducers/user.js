68851602e2a44fbce7892e66bc6cbefef7fc2dbb
hyoni0817 <dudu1104@naver.com>
2021-05-11 19:54:36 +0900

    Add: 로그인 관련 액션 및 리듀서

--- front/reducers/user.js
+++ front/reducers/user.js
@@ -1,9 +1,13 @@
 export const initialState = {
+    me: null,
     isSignedUp: false,
     isSigningUp: false,
     isUserIdChecked: false,
     isUserIdChecking: false,
     existingUserId: false,
+    isLoggingIn: false,
+    isLoggedIn: false,
+    loginErrorReason: '',
     userIdCheckReason: '',
     signUpErrorReason: '',
 };
@@ -17,6 +21,11 @@ export const SIGN_UP_FAILURE = 'SIGN_UP_FAILURE';
 export const USER_ID_CHECK_REQUEST = 'USER_ID_CHECK_REQUEST';
 export const USER_ID_CHECK_SUCCESS = 'USER_ID_CHECK_SUCCESS';
 export const USER_ID_CHECK_FAILURE = 'USER_ID_CHECK_FAILURE';
+
+//로그인 액션
+export const LOG_IN_REQUEST = 'LOG_IN_REQUEST';
+export const LOG_IN_SUCCESS = 'LOG_IN_SUCCESS';
+export const LOG_IN_FAILURE = 'LOG_IN_FAILURE';
 
 const reducer = ( state = initialState, action ) => {
     switch (action.type) {
@@ -57,6 +66,26 @@ const reducer = ( state = initialState, action ) => {
                 ...state,
                 isUserIdChecking: false,
                 userIdCheckReason: action.error,
+            };
+        case LOG_IN_REQUEST: 
+            return {
+                ...state,
+                isLoggingIn: true,
+                loginErrorReason: '',
+            };
+        case LOG_IN_SUCCESS: 
+            return {
+                ...state,
+                isLoggingIn: false,
+                isLoggedIn: true,
+                me: action.data,
+            };  
+        case LOG_IN_FAILURE: 
+            return {
+                ...state,
+                isLoggingIn: false,
+                me: null,
+                loginErrorReason: action.error,
             };
         default: {
             return {
