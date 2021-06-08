ff89bfe3d0017172a02ca348c48cc0a4a70e373e
hyoni0817 <dudu1104@naver.com>
2021-06-08 17:23:44 +0900

    Update: 정중앙에 로딩표시가 오도록 수정 및 로그아웃 시 로딩화면을 전체화면으로 표시
    - props로 전달받은 logOut을 통해 로그아웃 상태를 구별하여 로딩 화면을 나타냄.

--- front/components/Loading.js
+++ front/components/Loading.js
@@ -4,21 +4,31 @@ import { LoadingOutlined } from '@ant-design/icons';
 import { Spin } from 'antd';
 
 
-const LoadingView = styled.div`
-    color: #7262fd;
-    width: 100%;
-    height: auto;
-    text-align: center;
-    margin-top: 40vmin;
-    display: inline-block;
 
-    @media(max-width:420px) {
-        margin-top: 70vmin;
-    }
-`;
-
-const Loading = () => {
+const Loading = ({ logOut }) => {
     const antIcon = <LoadingOutlined style={{ fontSize: 45, color: '#7262fd' }} spin />;
+
+    const LoadingView = styled.div`
+        margin-left: ${ logOut ? '0' : '150px'};
+        color: #7262fd;
+        width: ${ logOut ? '100%' : 'none' };
+        height: ${ logOut ? '100%' : 0 };
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        background: ${logOut ? 'white' : 'none'};
+        text-align: center;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+
+        @media(max-width:767px) {
+            margin-left: 0;
+            z-index: ${logOut ? '1000' : '0'};
+        }
+    `;
 
     return (
         <>
