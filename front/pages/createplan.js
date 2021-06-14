536bd3c4c4476a62368a016ecf0f2387c91a993e
hyoni0817 <dudu1104@naver.com>
2021-06-15 02:46:37 +0900

    Add: 계획 작성 페이지에서 다른 페이지로 이동할 때 로딩 화면 구현

--- front/pages/createplan.js
+++ front/pages/createplan.js
@@ -1,9 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { useRouter } from 'next/router';
 import MobileForm from '../containers/MobileForm';
 import MobileFormPortal from '../components/MobileFormPortal';
 import DesktopForm from '../containers/DesktopForm';
+import Loading from '../components/Loading';
 
 //redux
 import { useSelector } from 'react-redux';
@@ -25,6 +26,8 @@ const CreatePlan = () => {
 
     const { me } = useSelector(state => state.user);
 
+    const [pageLoading, setPageLoading] = useState(false);
+
     useEffect(() => {
       router.prefetch('/home')
     }, [])
@@ -36,12 +39,26 @@ const CreatePlan = () => {
         }
     }, [me && me.id])
 
+    useEffect(() => {
+        if(!me) {
+            const handleStart = () => { setPageLoading(true); };
+            const handleComplete = () => { setPageLoading(false); };
+            router.events.on('routeChangeStart', handleStart);
+            router.events.on('routeChangeComplete', handleComplete);
+            router.events.on('routeChangeError', handleComplete);
+        }
+    }, [router && me]);
+
     return (
         <>
-            <MobileFormPortal selector="#mobile-form">
-                <MobileFormWrapper />
-            </MobileFormPortal>
-            <DesktopForm />
+            { pageLoading ? <><Loading logOut={true} /></> :
+                <>
+                    <MobileFormPortal selector="#mobile-form">
+                        <MobileFormWrapper />
+                    </MobileFormPortal>
+                    <DesktopForm />
+                </>
+            }
         </>
     );
 };
