f48447e6af9019f1e9ab21ff1d153c0b8a5a69df
hyoni0817 <dudu1104@naver.com>
2021-06-22 18:25:03 +0900

    Fix: 배포 후 로그아웃 할 때 로딩 화면이 전체 화면으로 표현안돼서 코드 수정
    - 기존에 전달받은 logOut props를 바로 사용하던 방식에서 스타일드 컴포넌트의 props를 통해 넘겨주는 방식을 사용함.

--- front/components/Loading.js
+++ front/components/Loading.js
@@ -3,36 +3,34 @@ import styled from 'styled-components';
 import { LoadingOutlined } from '@ant-design/icons';
 import { Spin } from 'antd';
 
+const LoadingView = styled.div`
+    margin-left: ${ props => props.logOut ? '0' : '150px'};
+    color: #7262fd;
+    width: ${ props => props.logOut ? '100%' : 'none' };
+    height: ${ props => props.logOut ? '100%' : 0 };
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    background: ${props => props.logOut ? 'white' : 'none'};
+    text-align: center;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
 
+    @media(max-width:767px) {
+        margin-left: 0;
+        z-index: ${props => props.logOut ? '1000' : '0'};
+    }
+`;
 
 const Loading = ({ logOut }) => {
     const antIcon = <LoadingOutlined style={{ fontSize: 45, color: '#7262fd' }} spin />;
-
-    const LoadingView = styled.div`
-        margin-left: ${ logOut ? '0' : '150px'};
-        color: #7262fd;
-        width: ${ logOut ? '100%' : 'none' };
-        height: ${ logOut ? '100%' : 0 };
-        position: absolute;
-        left: 50%;
-        top: 50%;
-        transform: translate(-50%, -50%);
-        background: ${logOut ? 'white' : 'none'};
-        text-align: center;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex-direction: column;
-
-        @media(max-width:767px) {
-            margin-left: 0;
-            z-index: ${logOut ? '1000' : '0'};
-        }
-    `;
-
+    
     return (
         <>
-            <LoadingView>
+            <LoadingView logOut={logOut}>
                 <Spin indicator={antIcon} />
                 <p style={{ marginTop:'15px' }}>잠시만 기다려주세요...</p>
             </LoadingView>
