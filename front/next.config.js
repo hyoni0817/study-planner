9fb50277884190d7bcfd8855054c66b4c6f059df
hyoni0817 <dudu1104@naver.com>
2021-09-01 23:30:02 +0900

    Add: 번들 분석 플러그인

--- front/next.config.js
+++ front/next.config.js
@@ -1,8 +1,24 @@
 const dotenv = require('dotenv');
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
 
 dotenv.config();
 
-module.exports = {
+module.exports = withBundleAnalyzer({
+    compress: true,
+    webpack(config, { webpack }) {
+      const prod = process.env.NODE_ENV === 'production';
+      return {
+        ...config,
+        mode: prod ? 'production' : 'development',
+        devtool: prod ? 'hidden-source-map' : 'eval',
+        plugins: [
+          ...config.plugins,
+          new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /^\.\/ko$/),
+        ],
+      }
+    },
     images: {
       domains: ['devwebdata2021.s3.ap-northeast-2.amazonaws.com'],
     },
@@ -12,4 +28,4 @@ module.exports = {
     async redirects() {
       return [];
     },
-  }
+  })
